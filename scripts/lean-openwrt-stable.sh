ba3550758f0f11296bad7e00c8250c0448d67c9f
wwqgtxx <wwqgtxx@gmail.com>
2020-07-08 22:51:13 +0800

    move my own fork
    Signed-off-by: wwqgtxx <wwqgtxx@gmail.com>

--- scripts/lean-openwrt-stable.sh
+++ scripts/lean-openwrt-stable.sh
@@ -22,7 +22,7 @@ mkdir package/community
 pushd package/community
 
 # Add Lienol's Packages
-git clone --depth=1 https://github.com/Lienol/openwrt-package
+git clone --depth=1 https://github.com/wwqgtxx-openwrt/lienol-package
 
 # Add mentohust & luci-app-mentohust.
 git clone --depth=1 https://github.com/BoringCat/luci-app-mentohust
