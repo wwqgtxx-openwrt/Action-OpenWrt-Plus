71c8a4101d0a843b8a2dafb0d0a46e34acb95195
wwqgtxx <wwqgtxx@gmail.com>
2020-07-10 15:28:56 +0800

    merge all stable repo to my namespace

--- scripts/lean-openwrt-stable.sh
+++ scripts/lean-openwrt-stable.sh
@@ -14,7 +14,7 @@
 
 # Add Project OpenWrt's autocore
 #rm -rf autocore
-#svn co https://github.com/project-openwrt/openwrt/branches/18.06-kernel5.4/package/lean/autocore
+#svn co https://github.com/wwqgtxx-openwrt/project-openwrt/branches/18.06-kernel5.4/package/lean/autocore
 #popd
 
 # Clone community packages to package/community
@@ -25,80 +25,80 @@ pushd package/community
 git clone --depth=1 https://github.com/wwqgtxx-openwrt/lienol-package
 
 # Add mentohust & luci-app-mentohust.
-git clone --depth=1 https://github.com/BoringCat/luci-app-mentohust
-git clone --depth=1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk
+git clone --depth=1 https://github.com/wwqgtxx-openwrt/luci-app-mentohust
+git clone --depth=1 https://github.com/wwqgtxx-openwrt/MentoHUST-OpenWrt-ipk
 
 # Add ServerChan
-git clone --depth=1 https://github.com/tty228/luci-app-serverchan
+git clone --depth=1 https://github.com/wwqgtxx-openwrt/luci-app-serverchan
 
 # Add OpenClash
-git clone --depth=1 -b master https://github.com/vernesong/OpenClash
+git clone --depth=1 -b master https://github.com/wwqgtxx-openwrt/OpenClash
 
 # Add luci-app-onliner (need luci-app-nlbwmon)
-git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
+git clone --depth=1 https://github.com/wwqgtxx-openwrt/luci-app-onliner
 
 # Add luci-app-adguardhome
-git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome
+git clone --depth=1 https://github.com/wwqgtxx-openwrt/luci-app-adguardhome
 
 # Add luci-app-diskman
-git clone --depth=1 https://github.com/lisaac/luci-app-diskman
+git clone --depth=1 https://github.com/wwqgtxx-openwrt/luci-app-diskman
 mkdir parted
 cp luci-app-diskman/Parted.Makefile parted/Makefile
 
 # Add luci-app-dockerman
-git clone --depth=1 https://github.com/KFERMercer/luci-app-dockerman
+git clone --depth=1 https://github.com/wwqgtxx-openwrt/luci-app-dockerman
 mkdir luci-lib-docker
-curl -s -o ./luci-lib-docker/Makefile https://raw.githubusercontent.com/lisaac/luci-lib-docker/master/Makefile
+curl -s -o ./luci-lib-docker/Makefile https://raw.githubusercontent.com/wwqgtxx-openwrt/luci-lib-docker/master/Makefile
 rm -rf ../lean/luci-app-docker
 
 # Add luci-app-gowebdav
-git clone --depth=1 https://github.com/project-openwrt/openwrt-gowebdav
+git clone --depth=1 https://github.com/wwqgtxx-openwrt/openwrt-gowebdav
 
 # Add luci-app-jd-dailybonus
-git clone --depth=1 https://github.com/jerrykuku/node-request
-git clone --depth=1 https://github.com/jerrykuku/luci-app-jd-dailybonus
+git clone --depth=1 https://github.com/wwqgtxx-openwrt/node-request
+git clone --depth=1 https://github.com/wwqgtxx-openwrt/luci-app-jd-dailybonus
 
 # Add luci-theme-argon
 #git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
 #rm -rf ../lean/luci-theme-argon
 
 # Add tmate
-svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/tmate
-svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/msgpack-c
+svn co https://github.com/wwqgtxx-openwrt/project-openwrt/trunk/package/ctcgfw/tmate
+svn co https://github.com/wwqgtxx-openwrt/project-openwrt/trunk/package/ctcgfw/msgpack-c
 
 # Add gotop
-svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/gotop
+svn co https://github.com/wwqgtxx-openwrt/project-openwrt/trunk/package/ctcgfw/gotop
 
 # Subscribe converters
-#svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/subconverter
-#svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/jpcre2
-#svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/rapidjson
-#svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/duktape
+#svn co https://github.com/wwqgtxx-openwrt/project-openwrt/trunk/package/ctcgfw/subconverter
+#svn co https://github.com/wwqgtxx-openwrt/project-openwrt/trunk/package/ctcgfw/jpcre2
+#svn co https://github.com/wwqgtxx-openwrt/project-openwrt/trunk/package/ctcgfw/rapidjson
+#svn co https://github.com/wwqgtxx-openwrt/project-openwrt/trunk/package/ctcgfw/duktape
 
 # Add smartdns
-svn co https://github.com/pymumu/smartdns/trunk/package/openwrt ../smartdns
-svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns ../luci-app-smartdns
+svn co https://github.com/wwqgtxx-openwrt/smartdns/trunk/package/openwrt ../smartdns
+svn co https://github.com/wwqgtxx-openwrt/project-openwrt/trunk/package/ntlf9t/luci-app-smartdns ../luci-app-smartdns
 
 # Add udptools
-git clone --depth=1 https://github.com/bao3/openwrt-udp2raw
-git clone --depth=1 https://github.com/bao3/openwrt-udpspeeder
-git clone --depth=1 https://github.com/bao3/luci-udptools
+git clone --depth=1 https://github.com/wwqgtxx-openwrt/openwrt-udp2raw
+git clone --depth=1 https://github.com/wwqgtxx-openwrt/openwrt-udpspeeder
+git clone --depth=1 https://github.com/wwqgtxx-openwrt/luci-udptools
 
 # Add OpenAppFilter
-git clone --depth=1 https://github.com/destan19/OpenAppFilter
+git clone --depth=1 https://github.com/wwqgtxx-openwrt/OpenAppFilter
 popd
 
 # Fix default-settings for local opkg sources
 sed -i '/http/d' package/lean/default-settings/files/zzz-default-settings
 
 # Fix libssh
-pushd feeds/packages/libs
-rm -rf libssh
-svn co https://github.com/openwrt/packages/trunk/libs/libssh
-popd
+#pushd feeds/packages/libs
+#rm -rf libssh
+#svn co https://github.com/openwrt/packages/trunk/libs/libssh
+#popd
 
 # Add po2lmo
-git clone https://github.com/openwrt-dev/po2lmo.git
+git clone https://github.com/wwqgtxx-openwrt/po2lmo.git
 pushd po2lmo
 make && sudo make install
 popd
